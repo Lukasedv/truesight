d6a3f837d6458af15312aaeef5ec3133cfe91c95
lukasedv <lukasedv@outlook.com>
2025-06-26 13:09:38 +0300

    chore: Clean up repository structure
    - Remove old lightroom-plugin.lrplugin directory and files
    - Remove obsolete documentation files
    - Update main README.md to reflect new plugin structure
    - Repository now uses missingopsin.lrplugin as the main plugin directory

--- lightroom-plugin.lrplugin/HelpConfiguration.lua
+++
@@ -1,9 +0,0 @@
---[[
-Help Menu Configuration Wrapper
-Ensures proper loading of configuration dialog from help menu
-]]
-
-local ConfigDialog = require 'ConfigDialog'
-
--- Direct execution when called from help menu
-ConfigDialog()
--- lightroom-plugin.lrplugin/HelpTroubleshooting.lua
+++
@@ -1,9 +0,0 @@
---[[
-Help Menu Troubleshooting Wrapper
-Ensures proper loading of troubleshooting dialog from help menu
-]]
-
-local TroubleShoot = require 'TroubleShoot'
-
--- Direct execution when called from help menu
-TroubleShoot()
